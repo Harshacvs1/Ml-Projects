ece56ccbae16ec70f9074c7f0cf2a8753b299e3b
C.V.S Harsha Vardhan <harshacvs1@gmail.com>
2024-11-02 23:40:42 +0530

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,27 @@
+import sys
+import logging
+
+def error_message_detail(error,error_deatil:sys):
+    # in exc_tb we store where error has occured and at which file every thing we will get
+    _,_,exc_tb = error_deatil.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = f"Error occured in python script name [{file_name}] line number [{exc_tb.tb_lineno}] error message [{str(error)}]"
+
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_details:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message,
+                                                  error_detail = error_details)
+        
+    def __str__(self) -> str:
+        return self.error_message
+    
+if __name__ == "__main__":
+
+    try:
+        a = 1/10
+    except Exception as e:
+        logging.INFO("Divide by zero")
+        raise CustomException(e,sys)
